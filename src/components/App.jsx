87f45a135562c67b8c64ffafc03b520c920352d1
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-09-18 11:17:44 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,73 @@
-import Gallery from './Gallery/Gallery';
 import './styles.css';
+import React, { useState, useEffect } from 'react';
+import { Searchbar } from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { Button } from 'components/Button/Button';
+import { Modal } from 'components/Modal/Modal';
+import { Audio } from 'react-loader-spinner';
 
-export const App = () => {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [inputSearch, setInputSearch] = useState('');
+  const [limit, setLimit] = useState(12);
+  const [page, setPage] = useState(1);
+  const [apiKey, setApiKey] = useState('36974281-9a9267ae338de1504a0765e3e');
+  const [clickedImage, setClickedImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async () => {
+      fetchImages();
+    };
+  }, []);
+
+  const fetchImages = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(
+        `https://pixabay.com/api/?q=${inputSearch}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${limit}`
+      );
+      const data = await response.json();
+
+      setImages(data.hits);
+      setIsLoading(false);
+    } catch (error) {
+      console.log('errr', error);
+      setIsLoading(false);
+    }
+  };
+
+  const submitHandler = evt => {
+    evt.preventDefault();
+
+    const form = evt.currentTarget;
+    const inputValue = form.elements.search.value;
+    setInputSearch(inputValue);
+    setPage(1);
+
+    () => {
+      fetchImages();
+    };
+  };
+
+  const loadMoreHandler = () => {
+    setLimit(limit + 12);
+    setPage(page + 1);
+
+    () => {
+      fetchImages();
+    };
+  };
+
+  const modalHandler = image => {
+    setClickedImage(image);
+  };
+
+  const closeModal = () => {
+    setClickedImage(null);
+  };
+
   return (
     <div
       className="App"
@@ -14,7 +80,26 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Gallery />
+      <Searchbar onSubmit={submitHandler} />
+      {inputSearch && (
+        <ImageGallery gallery={images} onClick={modalHandler}></ImageGallery>
+      )}
+      <Button onClick={loadMoreHandler} />
+      {clickedImage && <Modal image={clickedImage} onClose={closeModal} />}
+      {isLoading && (
+        <Audio
+          className="Spinner"
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      )}
     </div>
   );
 };
+
+export default App;
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,113 +1,104 @@
-import React, { Component } from 'react';
-import { Searchbar } from 'components/Searchbar/Searchbar';
-import { ImageGallery } from 'components/ImageGallery/ImageGallery';
-import { Button } from 'components/Button/Button';
-import { Modal } from 'components/Modal/Modal';
-import { Audio } from 'react-loader-spinner';
-import { useEffect, useState } from 'react';
+// import React, { Component } from 'react';
+// import { Searchbar } from 'components/Searchbar/Searchbar';
+// import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+// import { Button } from 'components/Button/Button';
+// import { Modal } from 'components/Modal/Modal';
+// import { Audio } from 'react-loader-spinner';
+// import { useEffect, useState } from 'react';
 
-class Gallery extends Component {
-  // state = {
-  //   images: [],
-  //   inputSearch: '',
-  //   limit: 12,
-  //   page: 1,
-  //   apiKey: '36974281-9a9267ae338de1504a0765e3e',
-  //   clickedImage: null,
-  //   isLoading: false,
-  // };
+// class Gallery extends Component {
+//   state = {
+//     images: [],
+//     inputSearch: '',
+//     limit: 12,
+//     page: 1,
+//     apiKey: '36974281-9a9267ae338de1504a0765e3e',
+//     clickedImage: null,
+//     isLoading: false,
+//   };
 
-  const [images, setImages] = useState([]);
-  const [inputSearch, setinputSearch] = useState('');
-  const [limit, setLimit] = useState(12);
-  const [page, setPage] = useState(1);
-  const [apiKey, setApiKey] = useState('36974281-9a9267ae338de1504a0765e3e');
-  const [clickedImage, setClickedImage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+//   async componentDidMount() {
+//     this.fetchImages();
+//   }
 
+//   fetchImages = async () => {
+//     try {
+//       const { inputSearch, apiKey, limit, page } = this.state;
 
-  async componentDidMount() {
-    this.fetchImages();
-  }
+//       this.setState({ isLoading: true });
 
-  fetchImages = async () => {
-    try {
-      const { inputSearch, apiKey, limit, page } = this.state;
+//       const response = await fetch(
+//         `https://pixabay.com/api/?q=${inputSearch}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${limit}`
+//       );
 
-      this.setState({ isLoading: true });
+//       const data = await response.json();
 
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${inputSearch}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${limit}`
-      );
+//       this.setState({ images: data.hits, isLoading: false });
+//     } catch (error) {
+//       console.log('errr', error);
+//       this.setState({ isLoading: false });
+//     }
+//   };
 
-      const data = await response.json();
+//   submitHandler = evt => {
+//     evt.preventDefault();
 
-      this.setState({ images: data.hits, isLoading: false });
-    } catch (error) {
-      console.log('errr', error);
-      this.setState({ isLoading: false });
-    }
-  };
+//     const form = evt.currentTarget;
+//     const inputValue = form.elements.search.value;
 
-  submitHandler = evt => {
-    evt.preventDefault();
+//     this.setState({ inputSearch: inputValue, page: 1 }, () => {
+//       this.fetchImages();
+//     });
+//   };
 
-    const form = evt.currentTarget;
-    const inputValue = form.elements.search.value;
+//   loadMoreHandler = () => {
+//     this.setState(
+//       prevState => ({ limit: prevState.limit + 12, page: prevState.page + 1 }),
+//       () => {
+//         this.fetchImages();
+//       }
+//     );
+//   };
 
-    this.setState({ inputSearch: inputValue, page: 1 }, () => {
-      this.fetchImages();
-    });
-  };
+//   modalHandler = image => {
+//     this.setState({ clickedImage: image });
+//   };
 
-  loadMoreHandler = () => {
-    this.setState(
-      prevState => ({ limit: prevState.limit + 12, page: prevState.page + 1 }),
-      () => {
-        this.fetchImages();
-      }
-    );
-  };
+//   closeModal = () => {
+//     this.setState({ clickedImage: null });
+//   };
 
-  modalHandler = image => {
-    this.setState({ clickedImage: image });
-  };
+//   render() {
+//     const { images, clickedImage, isLoading } = this.state;
 
-  closeModal = () => {
-    this.setState({ clickedImage: null });
-  };
+//     return (
+//       <div>
+//         <Searchbar onSubmit={this.submitHandler} />
+//         {this.state.inputSearch && (
+//           <ImageGallery
+//             gallery={images}
+//             onClick={this.modalHandler}
+//           ></ImageGallery>
+//         )}
+//         <Button onClick={this.loadMoreHandler} />
+//         {clickedImage && (
+//           <Modal image={clickedImage} onClose={this.closeModal} />
+//         )}
+//         {isLoading && (
+//           <Audio
+//             className="Spinner"
+//             height="80"
+//             width="80"
+//             radius="9"
+//             color="green"
+//             ariaLabel="loading"
+//             wrapperStyle
+//             wrapperClass
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  render() {
-    const { images, clickedImage, isLoading } = this.state;
-
-    return (
-      <div>
-        <Searchbar onSubmit={this.submitHandler} />
-        {this.state.inputSearch && (
-          <ImageGallery
-            gallery={images}
-            onClick={this.modalHandler}
-          ></ImageGallery>
-        )}
-        <Button onClick={this.loadMoreHandler} />
-        {clickedImage && (
-          <Modal image={clickedImage} onClose={this.closeModal} />
-        )}
-        {isLoading && (
-          <Audio
-            className="Spinner"
-            height="80"
-            width="80"
-            radius="9"
-            color="green"
-            ariaLabel="loading"
-            wrapperStyle
-            wrapperClass
-          />
-        )}
-      </div>
-    );
-  }
-}
-
-export default Gallery;
+// export default Gallery;
