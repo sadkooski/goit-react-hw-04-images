96c3423c0445eb3e191cc59bb3e9a883d6afede5
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-09-18 11:39:37 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,40 @@
-import './styles.css';
 import React, { useState, useEffect } from 'react';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modal/Modal';
 import { Audio } from 'react-loader-spinner';
+import './styles.css';
 
 const App = () => {
   const [images, setImages] = useState([]);
   const [inputSearch, setInputSearch] = useState('');
   const [limit, setLimit] = useState(12);
   const [page, setPage] = useState(1);
-  const [apiKey, setApiKey] = useState('36974281-9a9267ae338de1504a0765e3e');
+  const [apiKey] = useState('36974281-9a9267ae338de1504a0765e3e');
   const [clickedImage, setClickedImage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    async () => {
-      fetchImages();
+    const fetchImages = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          `https://pixabay.com/api/?q=${inputSearch}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${limit}`
+        );
+        const data = await response.json();
+
+        setImages(data.hits);
+        setIsLoading(false);
+      } catch (error) {
+        console.log('errr', error);
+        setIsLoading(false);
+      }
     };
-  }, []);
 
-  const fetchImages = async () => {
-    try {
-      setIsLoading(true);
-
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${inputSearch}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${limit}`
-      );
-      const data = await response.json();
-
-      setImages(data.hits);
-      setIsLoading(false);
-    } catch (error) {
-      console.log('errr', error);
-      setIsLoading(false);
-    }
-  };
+    fetchImages();
+  }, [inputSearch, page, apiKey, limit]);
 
   const submitHandler = evt => {
     evt.preventDefault();
@@ -45,19 +43,11 @@ const App = () => {
     const inputValue = form.elements.search.value;
     setInputSearch(inputValue);
     setPage(1);
-
-    () => {
-      fetchImages();
-    };
   };
 
   const loadMoreHandler = () => {
     setLimit(limit + 12);
     setPage(page + 1);
-
-    () => {
-      fetchImages();
-    };
   };
 
   const modalHandler = image => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
